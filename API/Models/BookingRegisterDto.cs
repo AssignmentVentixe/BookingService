f66be5996b82803e491e29103ce2f7358df44790
Joakim Erik Åkerman <joakim.akerman@hotmail.com>
2025-06-03 17:25:22 +0200

    fixed naming issue

--- API/Models/BookingRegisterDto.cs
+++ API/Models/BookingRegisterDto.cs
@@ -4,10 +4,8 @@ public class BookingRegisterDto
 {
     public string BookingEmail { get; set; } = null!;
     public string EventId { get; set; } = null!;
-
     public string EventName { get; set; } = null!;
     public string EventLocation { get; set; } = null!;
-    public decimal Price { get; set; }
     public DateTime EventDate { get; set; }
-
+    public decimal EventPrice { get; set; }
 }
